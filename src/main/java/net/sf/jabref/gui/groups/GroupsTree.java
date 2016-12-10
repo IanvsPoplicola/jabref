f3874cbe8431e51dc4521c057c47199f26e6a8eb
Tobias Diez <tobiasdiez@gmx.de>
2016-12-10 16:23:45 +0100

    Fix tests

--- src/main/java/net/sf/jabref/gui/groups/GroupsTree.java
+++ src/main/java/net/sf/jabref/gui/groups/GroupsTree.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Vector;
 
 import javax.swing.JTree;
